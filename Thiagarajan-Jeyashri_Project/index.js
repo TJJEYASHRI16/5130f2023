0d18c99598c833b841388bbe59c738746c06262b
TJJEYASHRI16 <114823525+TJJEYASHRI16@users.noreply.github.com>
2023-11-06 20:34:27 -0500

    V2

--- Thiagarajan-Jeyashri_Project/index.js
+++ Thiagarajan-Jeyashri_Project/index.js
@@ -1,26 +1,56 @@
 import express from "express";
-
-const app = express();
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import path from 'path';
 const port = 3011;
-app.use(express.static("public/public"));
+const app = express();
+app.use(express.static("public/"));
 
+const exportPath = path.join(process.cwd(), 'export');
+
+const exportAllPages = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Get all EJS template names
+  const ejsTemplates = fs.readdirSync('./views').filter(file => file.endsWith('.ejs'));
+
+
+  // Export each EJS template to static HTML
+  for (const ejsTemplate of ejsTemplates) {
+    const htmlTemplateName = ejsTemplate.replace('.ejs', '.html');
+
+    try {
+      await page.goto(`http://localhost:${port}/${ejsTemplate.replace('.ejs', '')}`);
+      const content = await page.content();
+      const outputPath = path.join(exportPath, htmlTemplateName);
+      fs.writeFileSync(outputPath, content);
+      console.log(`Exported ${htmlTemplateName}`);
+    } catch (error) {
+      console.error(`Error exporting ${htmlTemplateName}:`, error);
+    }
+  }
+
+  await browser.close();
+};
+
+app.get('/export', async (req, res) => {
+  await exportAllPages();
+  res.send('HTML export complete.');
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-//HOME page
-app.get("/",(req,res) =>{
-  res.render("index.ejs");
-})
-
-app.get("/about",(req,res) =>{
-  res.render("about.ejs");
-})
-
-app.get("/contact",(req,res) =>{
-  res.render("contact.ejs");
-})
-
-app.get("/",(req,res) =>{
-  res.render("index.ejs");
-})
